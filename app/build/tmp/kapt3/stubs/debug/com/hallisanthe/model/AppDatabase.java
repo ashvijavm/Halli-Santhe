99cbdfbf66c5e0a291003294204bde6330da25f4
Pavan <pavansn.2510@gmail.com>
2026-05-14 01:06:55 +0530

    chore: clean up build intermediates and update resource blame logs

--- app/build/tmp/kapt3/stubs/debug/com/hallisanthe/model/AppDatabase.java
+++ app/build/tmp/kapt3/stubs/debug/com/hallisanthe/model/AppDatabase.java
@@ -1,6 +1,6 @@
 package com.hallisanthe.model;
 
-@kotlin.Metadata(mv = {2, 2, 0}, k = 1, xi = 48, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0002\b\'\u0018\u0000 \u00062\u00020\u0001:\u0001\u0006B\u0007\u00a2\u0006\u0004\b\u0002\u0010\u0003J\b\u0010\u0004\u001a\u00020\u0005H&\u00a8\u0006\u0007"}, d2 = {"Lcom/hallisanthe/model/AppDatabase;", "Landroidx/room/RoomDatabase;", "<init>", "()V", "productDao", "Lcom/hallisanthe/model/ProductDao;", "Companion", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b\'\u0018\u0000 \u00052\u00020\u0001:\u0001\u0005B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0003\u001a\u00020\u0004H&\u00a8\u0006\u0006"}, d2 = {"Lcom/hallisanthe/model/AppDatabase;", "Landroidx/room/RoomDatabase;", "()V", "productDao", "Lcom/hallisanthe/model/ProductDao;", "Companion", "app_debug"})
 @androidx.room.Database(entities = {com.hallisanthe.model.Product.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends androidx.room.RoomDatabase {
     @kotlin.jvm.Volatile()
@@ -16,7 +16,7 @@ public abstract class AppDatabase extends androidx.room.RoomDatabase {
     @org.jetbrains.annotations.NotNull()
     public abstract com.hallisanthe.model.ProductDao productDao();
     
-    @kotlin.Metadata(mv = {2, 2, 0}, k = 1, xi = 48, d1 = {"\u0000\u001a\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\b\u0086\u0003\u0018\u00002\u00020\u0001B\t\b\u0002\u00a2\u0006\u0004\b\u0002\u0010\u0003J\u000e\u0010\u0006\u001a\u00020\u00052\u0006\u0010\u0007\u001a\u00020\bR\u0010\u0010\u0004\u001a\u0004\u0018\u00010\u0005X\u0082\u000e\u00a2\u0006\u0002\n\u0000\u00a8\u0006\t"}, d2 = {"Lcom/hallisanthe/model/AppDatabase$Companion;", "", "<init>", "()V", "INSTANCE", "Lcom/hallisanthe/model/AppDatabase;", "getInstance", "ctx", "Landroid/content/Context;", "app_debug"})
+    @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u001a\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\b\u0086\u0003\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u000e\u0010\u0005\u001a\u00020\u00042\u0006\u0010\u0006\u001a\u00020\u0007R\u0010\u0010\u0003\u001a\u0004\u0018\u00010\u0004X\u0082\u000e\u00a2\u0006\u0002\n\u0000\u00a8\u0006\b"}, d2 = {"Lcom/hallisanthe/model/AppDatabase$Companion;", "", "()V", "INSTANCE", "Lcom/hallisanthe/model/AppDatabase;", "getInstance", "ctx", "Landroid/content/Context;", "app_debug"})
     public static final class Companion {
         
         private Companion() {
